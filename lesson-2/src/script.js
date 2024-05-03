331330e9923fac9c6ebb4c2ba438c34899499c7c
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-05-03 22:18:22 +0300

    feat: complete lesson 2

--- lesson-2/src/script.js
+++ lesson-2/src/script.js
@@ -9,10 +9,61 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+
+// Positioning
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = -1
+// mesh.position.set(0.7,-0.6,-1)
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.6
+// mesh.scale.set(2,0.5,0.5)
+
+// Rotation
+// mesh.rotation.reorder("YXZ")
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+// Group
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 0.5
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 =  new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+const cube2 =  new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+
+cube2.position.x = -2
+
+const cube3 =  new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+
+cube3.position.x = 2
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+// scene.add(mesh)
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -27,8 +78,11 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.position.y = 1
+// camera.position.x = 1
 scene.add(camera)
 
+// camera.lookAt(mesh.position)
 /**
  * Renderer
  */
