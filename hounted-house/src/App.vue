1597ca0d6584c621d226770dc9c2818fba667c0c
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-06-12 22:14:26 +0300

    feat: init hounted house

---
+++ hounted-house/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <TresCanvas window-size clear-color="#82DBC5">
+    <TresPerspectiveCamera
+      :position="[3, 3, 3]"
+      :look-at="[0, 0, 0]"
+    />
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+  </TresCanvas>
+</template>
+
--- lesson-4/src/script.js
+++ lesson-4/src/script.js
@@ -1,4 +1,17 @@
 import * as THREE from 'three'
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - 0.5
+    cursor.y = -(e.clientY / sizes.height - 0.5)
+    console.log(cursor.x, cursor.y)
+})
 
 /**
  * Base
@@ -9,7 +22,7 @@ const canvas = document.querySelector('canvas.webgl')
 // Sizes
 const sizes = {
     width: 800,
-    height: 600
+    height: 400
 }
 
 // Scene
@@ -22,11 +35,13 @@ const mesh = new THREE.Mesh(
 )
 scene.add(mesh)
 
+const aspectRatio = sizes.width / sizes.height
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -44,7 +59,13 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    camera.position.x = Math.sin(cursor.x * 10) * Math.PI / 2
+    camera.position.z = Math.cos(cursor.x * 10) * Math.PI / 2
+    camera.position.y = cursor.y * 6
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
